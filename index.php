53802ecbc8b98726d1642d59561a4547ebf1f1c2
mezrioui hakim <mezrioui.hakim@gmail.com>
2024-03-26 00:22:36 +0000

    clarfication

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once "./Router.php";
 // Router::handle("GET", "/48_package/test", function() {
 //     echo "I'm a closur function, it works";
 // });
-function testMe() {
-    echo "this works as well";
-}
+// function testMe() {
+//     echo "this works as well";
+// }
 // Router::handle("GET", "/48_package/test", "testMe");
-Router::get("/48_package/test", "testMe");
+// Router::get("/48_package/test", "testMe");
